07c12af21e154ced5daba47bf91272e021a972a8
Marisa Vieira <marisavrst@gmail.com>
2024-06-09 11:35:39 -0300

    Adição da funcionalidade de exclusão de comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment(props){
+export function Comment({content, onDeleteComment}){
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/albert-medeiros.png"/>
@@ -16,12 +20,12 @@ export function Comment(props){
                             <time title="11 de Maio as 08:13" dateTime='2022-05-11 08:13:38'> Cerca de 1h atrás </time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24}/>
                         </button>
                     </header>
 
-                    <p>{props.content}</p>
+                    <p>{content}</p>
                 </div>
                 <footer>
                     <button>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -41,6 +41,13 @@ export function Post({author, publishedAt, content}){
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment != commentToDelete;
+        })
+        setComments(commentsWithoutDeletedOne);
+    }
+
     return(
         <article className={styles.post}>
             <header>
@@ -60,9 +67,9 @@ export function Post({author, publishedAt, content}){
             <div className={styles.content}>
                 {content.map(line=>{
                     if (line.type == 'paragraph'){
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     }else if (line.type == 'link'){
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
                     }
                 })}
             </div>
@@ -84,7 +91,7 @@ export function Post({author, publishedAt, content}){
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment}/>
+                    return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
                 })}
             </div>
         </article>
